e633eca74280323ded2ff12a195069d6330f4ab9
Gaël Heridet <gael@lespolypodes.com>
2015-08-26 15:23:47 +0200

    add twigs view for post list and show, with right override

--- src/Application/Sonata/NewsBundle/Controller/PostController.php
+++ src/Application/Sonata/NewsBundle/Controller/PostController.php
@@ -2,10 +2,11 @@
 
 namespace Application\Sonata\NewsBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Application\Sonata\NewsBundle\Entity\Post;
 
-class PostController extends Controller
+use Sonata\NewsBundle\Controller\PostController as BaseController;
+
+class PostController extends BaseController
 {
     public function indexAction(){
         $news = $this->getDoctrine()
@@ -15,18 +16,19 @@ class PostController extends Controller
         return $this->render('ApplicationSonataNewsBundle:Post:index.html.twig', compact('news'));
     }
 
-    public function showAction($id){
-        $actu = $this->getDoctrine()
-                    ->getRepository('ApplicationSonataNewsBundle:Post')
-                    ->find($id)
-                ;
-        if (!$actu) {
-            throw $this->createNotFoundException(
-                'Aucun produit trouvé pour cet id : '.$id
-            );
-        }
-        return $this->render('ApplicationSonataNewsBundle:Post:show.html.twig', compact('actu'));
-    }
+    // public function showAction($id){
+    //     $actu = $this->getDoctrine()
+    //                 ->getRepository('ApplicationSonataNewsBundle:Post')
+    //                 ->find($id)
+    //             ;
+    //     if (!$actu) {
+    //         throw $this->createNotFoundException(
+    //             'Aucun produit trouvé pour cet id : '.$id
+    //         );
+    //     }
+    //     return $this->render('ApplicationSonataNewsBundle:Post:show.html.twig', compact('actu'));
+    // }
+
 }
 
  ?>
