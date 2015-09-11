d1173c0d295549c268c1245ecd4e186aa533ec24
Olivier Bouchet <olivier.bouchet@gmail.com>
2015-09-11 10:43:13 +0200

    Modified live stream popup to load only template without layout in AJAX request

--- src/RadioSolution/ProgramBundle/Controller/EcouteController.php
+++ src/RadioSolution/ProgramBundle/Controller/EcouteController.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use RadioSolution\ProgramBundle\Entity\Emission;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Emission controller.
@@ -18,16 +19,12 @@ use RadioSolution\ProgramBundle\Entity\Emission;
 class EcouteController extends Controller
 {
 
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $program = null;
         $onair = null;
 
         $em = $this->getDoctrine()->getEntityManager();
-
-        $url = $this->container->getParameter('nowPlayingUrl');
-        $file = fopen($url, 'r');
-        $content = fgets($file);
 
         $program = $em
             ->createQuery("SELECT p FROM ProgramBundle:Program p WHERE p.time_stop > :now AND p.time_start >= :now AND p.time_start < p.time_stop  ORDER BY p.time_start ASC, p.time_stop DESC")
@@ -109,6 +106,7 @@ class EcouteController extends Controller
 
     	$nowSlide = array_search($currentProgram[0], $entities);
         */
+
 		return $this->render('ProgramBundle:Program:show_ecoute.html.twig', compact('program', 'onair', 'broadcasts'));
     }
 
