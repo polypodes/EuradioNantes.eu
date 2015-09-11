d8b4ba391fc21efb1090e1b809cbb9544db0bbc0
Olivier Bouchet <olivier.bouchet@gmail.com>
2015-09-11 15:49:47 +0200

    Set env to prod in main web/app.php file

--- web/app.php
+++ web/app.php
@@ -20,8 +20,8 @@ Debug::enable();
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-//$kernel = new AppKernel('prod', false);
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', false);
+//$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
